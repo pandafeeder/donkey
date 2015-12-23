6c44a4dce6267df25634f7b38e5a2caaeb812290
PandaFeeder <pandafeeder@gmail.com>
2015-12-24 00:21:31 +0800

    add views file for rendering pages

--- builder/views.py
+++ builder/views.py
@@ -1,5 +1,28 @@
-from django.http import HttpResponse
+import os
+
+from django.http import Http404
+from django.shortcuts import render_to_response
+from django.conf import settings
+from django.template import Template, Context
 
 
-def index(request):
-    return HttpResponse("INDEX")
+def get_page_or_404(pagename):
+    """get the content page then Template it"""
+    pagedir = settings.SITE_PAGE_DIRECTORY
+    pagefile_path = os.path.join(pagedir, pagename)
+    if not os.path.exists(pagefile_path):
+	raise Http404("Page does not exist")
+    with open(pagefile_path, 'r') as f:
+	page = Template(f.read())
+    return page
+
+
+def page(request, slug='index'):
+    """render each page"""
+    pagename = '{}.html'.format(slug)
+    page = get_page_or_404(pagename)
+    context = {
+	'page': page,
+	'slug': slug,
+    }
+    return render_to_response('page.html', context)
